69f2dbc16500fffe4b340a50fdd96010d4a5c18c
Parth Kukadiya <your-email@example.com>
2025-09-01 22:24:01 +0530

    Initial commit with C tutorial files

--- arrayasfunctionargument.c
+++ arrayasfunctionargument.c
@@ -1,15 +1,13 @@
-#include<stdio.h>// array as function argument.
-
-void print_Numbers(int arr[] , int n);
+# include<stdio.h>
+void printnumbers(int arr[], int n);
 int main() {
     int arr[]= {1, 2, 3, 4, 5, 6};
-    print_Numbers(arr, 6);
-    
+    printnumbers(arr, 6);
     return 0;
 }
-void print_Numbers(int arr[], int n){
+void printnumbers(int arr[], int n){
     for(int i=0;i<n;i++){
         printf("%d \t", arr[i]);
-}
-printf("\n");
+    }
+    printf("\n");
 }
