80141cf0c8a8880586dae268680ed9ea4ed436c8
Parth Kukadiya <your-email@example.com>
2025-09-02 23:27:03 +0530

    Initial commit with C tutorial files

--- Projects in C/Project3.c
+++ Projects in C/Project3.c
@@ -1,60 +1,84 @@
 # include<stdio.h>// calender of the  1 year.
 int main() {
     int i, a;
- 
+    int x=0,r=0;
         for(int i=1;i<=7;i++){
             int a=1;
-            
+            r=0;    
             if(i==1){
                 printf("january \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("  M  T  W  T  F  S  S\n");
             }
             if(i==2){
                 printf("february  \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==3){
-                printf("march   \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("\nmarch   \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==4){
                 printf("april \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==5){
-                printf("may \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("\nmay \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==6){
-                printf("june \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("\njune \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==7){
                 printf("july \n");
-                 printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
 
-            for(int i=1; i<=4;i++){
-            for(int i=1;i<=7;i++){
-                printf("%8d", a);
-                
-                 a++;
-                 }
-                 printf("\n");
-
-                
+            while(a<=28){
+                x=x%7;
+                for (int i = 0; i<x && a==1; i++)
+                {
+                    printf("   ");
+                    r++;
+                }
+                if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                    printf("%3d", a);
+                    x++;
+                    r++;
+                    a++;
             }
             
-            if (i!=2){
+
+            
+        if (i!=2){
         a=29;
-        printf("%8d", a);
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                r++;
+        printf("%3d", a);
+        x++;
        
         a=30;
-        printf("%8d", a);
-        
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                r++;
+        printf("%3d", a);
+        x++;
         if(i%2!=0){
         a=31;
-        printf("%8d \n", a);
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+        x++;r++;
+        printf("%3d \n", a);
        
         }
     }
@@ -63,51 +87,77 @@ int main() {
         
           for(int i=8;i<=12;i++){
             int a=1;
+            r=0;    
             if(i==8){
-                printf("august   \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("august \n");
+                printf("  M  T  W  T  F  S  S\n");
             }
             if(i==9){
-                printf("september \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("september  \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==10){
-                printf("october \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("\noctober \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==11){
-                printf("novemvber \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("november \n");
+                 printf("  M  T  W  T  F  S  S\n");
             }
             if(i==12){
-                printf("december \n");
-                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
+                printf("\ndecember \n");
+                 printf("  M  T  W  T  F  S  S\n");
+          }
+          while(a<=28){
+                x=x%7;
+                for (int i = 0; i<x && a==1; i++)
+                {
+                    printf("   ");
+                    r++;
+                }
+                if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                    printf("%3d", a);
+                    x++;
+                    r++;
+                    a++;
             }
-
-            for(int i=1; i<=4;i++){
-            for(int i=1;i<=7;i++){
-                printf("%8d ", a);
-              
-                a++;
-            }
-            printf("\n");
-        }
-        
             
-            if (i!=2){
-        int a=29;
-        printf("%8d ", a);
+
+            
+        if (i!=2){
+        a=29;
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                r++;
+        printf("%3d", a);
+        x++;
+       
         a=30;
-        printf("%8d  ", a);
-        if(i%2==0){
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+                r++;
+        printf("%3d", a);
+        x++;
+        if(i%2!=0){
         a=31;
-        printf("%8d ", a);
+        if(r==7){
+                    printf("\n");
+                    r=0;
+                }
+        x++;r++;
+        printf("%3d \n", a);
+       
         }
     }
-}
+            }
         
-      return 0;
-    }
-
-
-  
+        
+          return 0;
+        }
