51199a779de3fce10d8e977b262580d3ff6b2f0b
Parth Kukadiya <your-email@example.com>
2025-09-02 21:24:17 +0530

    Initial commit with C tutorial files

--- Projects in C/Project3.c
+++ Projects in C/Project3.c
@@ -65,39 +65,46 @@ int main() {
             int a=1;
             if(i==8){
                 printf("august   \n");
+                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
             }
             if(i==9){
                 printf("september \n");
+                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
             }
             if(i==10){
                 printf("october \n");
+                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
             }
             if(i==11){
                 printf("novemvber \n");
+                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
             }
             if(i==12){
                 printf("december \n");
+                printf(" monday   tuesday  wednesday thursday friday saturday sunday \n");
             }
 
             for(int i=1; i<=4;i++){
             for(int i=1;i<=7;i++){
-                printf("%d ", a);
+                printf("%8d ", a);
               
                 a++;
             }
+            printf("\n");
         }
-        }
+        
             
             if (i!=2){
         int a=29;
-        printf("%d ", a);
+        printf("%8d ", a);
         a=30;
-        printf("%d  ", a);
+        printf("%8d  ", a);
         if(i%2==0){
         a=31;
-        printf("%d ", a);
+        printf("%8d ", a);
         }
     }
+}
         
       return 0;
     }
