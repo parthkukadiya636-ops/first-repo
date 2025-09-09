b9d4cc47df7fb48f6074b87cdec495b7c2a7c055
Parth Kukadiya <your-email@example.com>
2025-09-09 18:55:04 +0530

    Initial commit with C tutorial files

---
+++ QUESTION58.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+
+void storetables(int tables[][10], int n, int m, int number);
+
+
+int main() {
+
+    int tables[2][10];
+    storetables(tables , 0, 10, 2);
+     storetables(tables , 1, 10, 3);
+    return 0;
+}
+
+void storetables(int tables[][10], int n, int m, int number){
+    for(int i=1 ; i<m ; i++){
+        tables[n][i] = number * i; 
+
+
+        printf("%d \t", tables[n][i]);
+    }
+}
