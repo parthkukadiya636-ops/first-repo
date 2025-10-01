73a518a2541678575aec3741301e2b2e0169896d
Parth Kukadiya <parthkukadiya636@gmail.com>
2025-10-01 17:40:42 +0530

    Initial commit with C tutorial files

---
+++ AREA.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+// area of rectangle
+int main() { 
+    int a;
+    printf("enter a");
+    scanf("%d", &a);
+    int b;
+    printf("enter b");
+    scanf("%d", &b);
+    int area= a*b;
+    printf("area is : %d", area);
+    return 0;
+
+}
