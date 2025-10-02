3ce29bb4dde26d128c7e618a19af010571d0afd8
Parth Kukadiya <parthkukadiya636@gmail.com>
2025-10-02 14:14:37 +0530

    Initial commit with C tutorial files

---
+++ ARRAY.c
@@ -0,0 +1,45 @@
+# include<stdio.h>
+int main( )
+{
+    // Array is structured data with same datatype in continues format.
+    int marks1= 97;
+    int marks2 = 98;
+    int marks3 = 99;// in place of three variable we can write it in one variable using array in structed way.
+
+    int marks[] = {97,98,99};
+
+    int marks[3];
+    
+    printf("enter marks of phy : ");
+    scanf("%d", &marks[0]);
+
+    printf("enter marks of chem : ");
+    scanf("%d", &marks[1]);
+
+    printf("enter marks of math: ");
+    scanf("%d", &marks[2]);
+
+    printf("phy =%d , chem =%d, math =%d", marks[0], marks[1], marks[2] );
+    return 0;
+    /* pointer arithmetic :
+
+    case 1:
+    pointer can be increament or decreament 
+    as an example :
+    int age = 22;
+    int *ptr = &age;
+    ptr ++; 
+    output will be the address of ptr wil be increasd by 4 by
+    tes as its data type is int .
+
+    case :2 
+    if it has float datatype it will be increased by float bytes.
+
+    case 3:
+     if it has character datatype it will be increased by character bytes.
+     
+     
+     we can  also substract one pointer from another.
+     we can also compare two pointer . */ 
+   
+}
