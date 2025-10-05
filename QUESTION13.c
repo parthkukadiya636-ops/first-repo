162d6259611ac91d54b4133295640b73492c62f5
Parth Kukadiya <parthkukadiya636@gmail.com>
2025-10-05 15:47:54 +0530

    Initial commit with C tutorial files

---
+++ QUESTION13.c
@@ -0,0 +1,9 @@
+# include<stdio.h>//print the number from 0 to n ,if n is given by user (n=4)
+int main() {
+    int i=0;
+    while(i<=4){
+        printf("%d \n", i);
+        i++;
+    }
+    return 0;
+}
