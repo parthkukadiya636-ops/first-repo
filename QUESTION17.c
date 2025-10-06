937647ba85e5b1f427da7e1444f34aa06b4d60e2
Parth Kukadiya <parthkukadiya636@gmail.com>
2025-10-06 13:31:26 +0530

    Initial commit with C tutorial files

---
+++ QUESTION17.c
@@ -0,0 +1,11 @@
+# include<stdio.h>// calculate the sum of all numbers between 5 and 50.
+int main() {
+    int sum,i;
+    sum =0;
+    for(i=6;i<=49;i++)
+    {
+        sum= sum + i;
+    }
+    printf(" sum is %d", sum);
+    return 0;
+}
