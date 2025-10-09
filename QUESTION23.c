aaf29fd5d3e0c3b5f26f722be777313379c737cf
Parth Kukadiya <parthkukadiya636@gmail.com>
2025-10-09 12:37:51 +0530

    Initial commit with C tutorial files

---
+++ QUESTION23.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+void printhello();
+void printGoodbye();
+
+int main() {
+    printhello();
+    printGoodbye();
+    return 0;
+
+}
+
+void printhello() {
+    printf("hello! \n");
+}
+void printGoodbye() {
+    printf(" Good bye \n");
+}
