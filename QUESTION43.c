7c28c3c49c7affc3164ece0e70d7019f03c2fb01
Parth kukadiya <parthkukadiya636@gmail.com>
2025-10-22 10:23:08 +0530

    Initial commit with C tutorial files

---
+++ QUESTION43.c
@@ -0,0 +1,15 @@
+# include<stdio.h>// print a pattern using nested loop.
+int main() {
+for(int i=1;i<=4;i++){
+
+    for(int i=1;i<=5;i++){
+        printf("%c", '*');
+        printf(" ");
+
+    }
+    printf("\n");
+}
+return 0;
+}
+   
+
