64ad8504e362d2268bed4662ef7e2421196fab66
Parth kukadiya <parthkukadiya636@gmail.com>
2025-10-24 22:00:02 +0530

    Initial commit with C tutorial files

---
+++ QUESTION45.c
@@ -0,0 +1,20 @@
+# include<stdio.h>// write a programe to find whether the number is prime number or not through loop.
+int main() {
+    int n,count=0;
+    printf("enter a number :");
+    scanf("%d", &n);
+
+   for( int i=2;i<=n;i++){
+    if(n%i==0){
+    count++;
+    }
+}
+    if(count ==2){
+        printf(" it is prime number");
+    }
+    else{
+        printf("it is not a prime number");
+    }
+   
+    return 0;
+}
