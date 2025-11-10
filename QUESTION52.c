2d3cfa08acf63f3f74019497123b8d2146f733fe
Parth kukadiya <parthkukadiya636@gmail.com>
2025-11-10 22:23:51 +0530

    Initial commit with C tutorial files

--- QUESTION52.c
+++
@@ -1,9 +0,0 @@
-# include<stdio.h>// to print the marks of subjects with the help of loop.
-int main() {
-    int marks[5]= {25,25,25,25,25};
-    for(int i=0;i<=5;i++){
-        printf("marks are : %d", marks[i]);
-    }
-     return 0;
-
-    }
